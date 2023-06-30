c07b75449c8e8f46a90c1dd4a4d8f79047b995af
eric.oliveira <olv.ericlopes@gmail.com>
2023-06-30 16:38:24 -0300

    creating new endpoints to search a list by name and salary

--- src/main/java/com/olvericc/jparepository/controllers/UserController.java
+++ src/main/java/com/olvericc/jparepository/controllers/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -28,6 +29,18 @@ public class UserController {
     @GetMapping(value = "/page")
     public ResponseEntity<Page<User>> findAll(Pageable pageable) {
         Page<User> result = repository.findAll(pageable);
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping(value = "/searchSalary")
+    public ResponseEntity<Page<User>> searchBySalary(@RequestParam(defaultValue = "0") Double minSalary, @RequestParam(defaultValue = "1000000000000") Double maxSalary, Pageable pageable) {
+        Page<User> result = repository.findBySalaryBetween(minSalary, maxSalary, pageable);
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping(value = "/searchByName")
+    public ResponseEntity<Page<User>> searchByName(@RequestParam(defaultValue = "") String name, Pageable pageable) {
+        Page<User> result = repository.findByNameContainingIgnoreCase(name, pageable);
         return ResponseEntity.ok(result);
     }
 
