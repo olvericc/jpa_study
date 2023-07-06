cd5c3a0e314790c1b2a323e859402960b7c1c949
eric.oliveira <olv.ericlopes@gmail.com>
2023-07-06 13:05:00 -0300

    adding a new endpoint searchPrice across minPrice and maxPrice

--- src/main/java/com/olvericc/jparepository/controllers/ProductController.java
+++ src/main/java/com/olvericc/jparepository/controllers/ProductController.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -32,6 +33,14 @@ public class ProductController {
     @GetMapping(value = "/page")
     public ResponseEntity<Page<Product>> findAll(Pageable pageable) {
         Page<Product> result = repository.findAll(pageable);
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping(value = "/searchPrice")
+    public ResponseEntity<Page<Product>> searchPrice(
+            @RequestParam(defaultValue = "0") Double minPrice,
+            @RequestParam(defaultValue = "1000000000000") Double maxPrice, Pageable pageable) {
+        Page<Product> result = repository.searchPrice(minPrice, maxPrice, pageable);
         return ResponseEntity.ok(result);
     }
 
--- src/main/java/com/olvericc/jparepository/repositories/ProductRepository.java
+++ src/main/java/com/olvericc/jparepository/repositories/ProductRepository.java
@@ -1,8 +1,14 @@
 package com.olvericc.jparepository.repositories;
 
 import com.olvericc.jparepository.entities.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Query("SELECT obj FROM Product obj WHERE obj.price >= :minPrice AND obj.price <= :maxPrice")
+    Page<Product> searchPrice(Double minPrice, Double maxPrice, Pageable pageable);
 
 }
