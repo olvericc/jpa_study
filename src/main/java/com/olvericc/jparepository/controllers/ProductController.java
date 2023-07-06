6d321314f1c32a2b14764ceb67e7c564de31314a
eric.oliveira <olv.ericlopes@gmail.com>
2023-07-06 14:30:27 -0300

    creating a new CreateProduct endpoint with @PostMapping

--- src/main/java/com/olvericc/jparepository/controllers/ProductController.java
+++ src/main/java/com/olvericc/jparepository/controllers/ProductController.java
@@ -7,10 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -64,6 +61,11 @@ public class ProductController {
             @RequestParam(defaultValue = "") Long id, Pageable pageable) {
         Page<Product> result = repository.searchById(id, pageable);
         return ResponseEntity.ok(result);
+    }
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product) {
+        return repository.save(product);
     }
 
 }
